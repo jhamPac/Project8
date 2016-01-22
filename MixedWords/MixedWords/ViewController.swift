44de605653195bba7e04d2ca867972e867306e4c
Kevia <cloudy2012@gmail.com>
2016-01-21 17:13:52 -0800

    start of project

---
+++ MixedWords/MixedWords/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MixedWords
+//
+//  Created by jhampac on 1/21/16.
+//  Copyright © 2016 jhampac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
